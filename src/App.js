a0558995736670e32254afab9cefc4f355053289
Yvonne Nkr <yvonnkr86@gmail.com>
2020-04-18 17:35:54 +0100

    Added Layout,Header,Sidebar,Footer components and HomeScreen, ProductScreen, Routing and Styling

--- src/App.js
+++ src/App.js
@@ -1,66 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import "./App.css";
+import Layout from "./components/Layout";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
-  const openMenu = () => {
-    document.querySelector(".sidebar").classList.add("open");
-  };
-
-  const closeMenu = () => {
-    document.querySelector(".sidebar").classList.remove("open");
-  };
-
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">amazona</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          x
-        </button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-        </ul>
-      </aside>
-
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">£45</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path="/product/:id" exact component={ProductScreen} />
+          <Route path="/" exact component={HomeScreen} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
